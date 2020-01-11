6b1f1b0f8b7e1a579542d9e2b4091b1a9ba42d7a
l.chilka <l.chilka@eldro-la.eldro1.local>
2020-01-11 23:53:49 +0100

    testing...

---
+++ Test/src/Test.java
@@ -0,0 +1,28 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		int[][] grid = {{0,1,0,0},{1,1,1,0},{0,1,0,0},{1,1,0,0}};
+		// TODO Auto-generated method stub
+		System.out.println(islandPerimeter(grid ));
+	}
+	
+	private static int islandPerimeter(int[][] grid) {
+        int islands = 0, neighbour = 0;
+        for(int i = 0; i<grid.length; i++){
+            for(int j = 0; j<grid[i].length; j++){
+                if(grid[i][j] == 1){
+                    islands++;
+                    if(i<grid.length-1 && grid[i+1][j] == 1){
+                        neighbour++;
+                    }
+                    if(j<grid[i].length-1 && grid[i][j+1] == 1){
+                        neighbour++;
+                    }
+                }
+            }
+        }
+        return (islands*4)-(neighbour*2);
+    }
+
+}
