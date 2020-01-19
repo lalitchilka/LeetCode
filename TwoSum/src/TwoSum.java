b80034136a2a92cb257257e13fffb745fffff1b7
l.chilka <l.chilka@eldro-la.eldro1.local>
2020-01-19 21:33:49 +0100

    1. Two Sum

---
+++ TwoSum/src/TwoSum.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nums = {2,11,7,15};
+		int target = 9;
+		int[] result = twoSum(nums, target);
+		for(int i = 0; i < 2; i++) {
+			System.out.println(result[i]);
+		}
+	}
+	
+	private static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i])) return new int[] {map.get(target - nums[i]), i};
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("Invalid Array");
+	}
+
+}
