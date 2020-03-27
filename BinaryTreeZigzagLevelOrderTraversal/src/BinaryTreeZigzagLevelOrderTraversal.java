5b4bbef79df0cac7da2e34dcc71d6669e2aa9eed
lalitchilka <lalitchilka@gmail.com>
2020-03-27 15:08:58 +0100

    103. Binary Tree Zigzag Level Order Traversal

---
+++ BinaryTreeZigzagLevelOrderTraversal/src/BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeZigzagLevelOrderTraversal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeNode t1 = new TreeNode(3);
+		TreeNode t2 = new TreeNode(9);
+		TreeNode t3 = new TreeNode(20);
+		TreeNode t4 = new TreeNode(15);
+		TreeNode t5 = new TreeNode(17);
+		t1.left = t2;
+		t1.right = t3;
+		t3.left = t4;
+		t3.right = t5;
+		System.out.println(zigzagLevelOrder(t1));
+	}
+	
+	private static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+		if(root == null) return new ArrayList<>();
+		Queue<TreeNode> q = new LinkedList<>();
+		List<List<Integer>> result = new ArrayList<>();
+		q.offer(root);
+		
+		while(!q.isEmpty()) {
+			int size = q.size();
+			List<Integer> l = new ArrayList<>();
+			for(int i = 0; i < size; i++) {
+				TreeNode node = q.poll();
+				if(node.left != null) q.offer(node.left);
+				if(node.right != null) q.offer(node.right);
+				if(result.size() % 2 == 0) l.add(node.val);
+				else l.add(0, node.val);
+			}
+			result.add(l);
+		}
+		return result;
+		
+	}
+
+}
---
+++ BinaryTreeZigzagLevelOrderTraversal/src/BinaryTreeZigzagLevelOrderTraversal2.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeZigzagLevelOrderTraversal2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeNode t1 = new TreeNode(3);
+		TreeNode t2 = new TreeNode(9);
+		TreeNode t3 = new TreeNode(20);
+		TreeNode t4 = new TreeNode(15);
+		TreeNode t5 = new TreeNode(17);
+		t1.left = t2;
+		t1.right = t3;
+		t3.left = t4;
+		t3.right = t5;
+		System.out.println(zigzagLevelOrder(t1));
+	}
+	
+	private static List<List<Integer>> zigzagLevelOrder(TreeNode root){
+		if(root == null) return new ArrayList<>();
+		Queue<TreeNode> q = new LinkedList<>();
+		List<List<Integer>> result = new ArrayList<>();
+		boolean zigzag = false;
+		q.offer(root);
+		
+		while(!q.isEmpty()) {
+			int size = q.size();
+			List<Integer> l = new ArrayList<>();
+			for(int i = 0; i < size; i++) {
+				TreeNode node = q.poll();
+				if(node.left != null) q.offer(node.left);
+				if(node.right != null) q.offer(node.right);
+				l.add(node.val);
+			}
+			if(zigzag) Collections.reverse(l);
+			zigzag = !zigzag;
+			result.add(l);
+		}
+		return result;
+	}
+
+}
